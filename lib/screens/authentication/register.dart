0bef1ad7f5aab7cd2e70f4e1e91944911aa43899
Nedim <mr.ramicevic@gmail.com>
2022-02-20 17:16:06 +0300

    registerDone

--- lib/screens/authentication/register.dart
+++ lib/screens/authentication/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_firebase/extensions/context_extension.dart';
+import 'package:todo_firebase/services/authentication.dart';
 import 'package:todo_firebase/shared/constants.dart';
 
 class Register extends StatefulWidget {
@@ -10,8 +11,11 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  String email = "";
+  String password = "";
   @override
   Widget build(BuildContext context) {
+    Authenticate auth = Authenticate();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sign In"),
@@ -25,23 +29,35 @@ class _RegisterState extends State<Register> {
             Form(
                 child: Column(
               children: [
-                SizedBox(
-                  height: context.dynamicHeight(0.05),
-                ),
+                context.emptyWidgetHeight,
                 TextFormField(
+                  validator: (value) {
+                    value!.isEmpty ? "unvalid email" : null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                    });
+                  },
                   decoration: textInputDecoration.copyWith(hintText: "E-mail"),
                 ),
-                SizedBox(
-                  height: context.dynamicHeight(0.05),
-                ),
+                context.emptyWidgetHeight,
                 TextFormField(
+                  validator: (value) =>
+                      value!.length <= 6 ? "at least 6 characters" : null,
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  },
                   decoration:
                       textInputDecoration.copyWith(hintText: "Password"),
                 ),
-                SizedBox(
-                  height: context.dynamicHeight(0.05),
-                ),
-                const ElevatedButton(onPressed: null, child: Text("Register"))
+                context.emptyWidgetHeight,
+                ElevatedButton(
+                    onPressed: () =>
+                        auth.createUserWithEmailandPassword(email, password),
+                    child: const Text("Register")),
               ],
             ))
           ],
---
+++ lib/services/authentication.dart
@@ -0,0 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Authenticate {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream<User> get user {
+    return _auth.authStateChanges();
+  }
+
+  Future signInWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = userCredential.user;
+      return user;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  Future createUserWithEmailandPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      User user = userCredential.user;
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      return e;
+    }
+  }
+}
