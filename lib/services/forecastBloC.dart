670eca04ac3386e244ed1a07bf8b85a79933366d
Nedim <mr.ramicevic@gmail.com>
2022-02-24 10:06:16 +0300

    convertingToChangeNotifier

--- lib/services/forecastBloC.dart
+++ lib/services/forecastBloC.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:todo_firebase/services/data_retrieve.dart';
 import 'package:todo_firebase/shared/constants.dart';
 
-class ForecastBloC {
+class ForecastBloC extends ChangeNotifier {
   final WeatherServise _weatherServise = WeatherServise();
 
-  Future<ForecastData> get fetch {
-    return _weatherServise.fetchForecast();
+  late final Future<ForecastData> forecastData;
+
+  void fetch(String city) {
+    forecastData = _weatherServise.fetchForecast(city);
+    notifyListeners();
   }
 }
 
